f3cb9417e7b197009526c4e85815c56492fe7ad5
Romulo Rocha <romulo@cactoit.com>
2023-07-04 12:29:50 -0300

    feat: this is a poc for edge side mfe routing templates

--- coders-root-config/src/coders-root-config.ts
+++ coders-root-config/src/coders-root-config.ts
@@ -4,17 +4,46 @@ import {
   constructRoutes,
   constructLayoutEngine,
 } from "single-spa-layout";
+
 import microfrontendLayout from "./microfrontend-layout.html";
 
-const routes = constructRoutes(microfrontendLayout);
-const applications = constructApplications({
-  routes,
-  loadApp({ name }) {
-    return System.import(name);
-  },
-});
-const layoutEngine = constructLayoutEngine({ routes, applications });
+function getTemplate(template){
+  console.log("template",template);
+  if(!template){
+    customStart(microfrontendLayout)
+  }
+  else {
+    fetch(`/template/${template}-layout.html`).then((res) => res.text()).then((res) => {
+      console.log(res);
+      customStart(res)
+      return res;
+    }).catch((err) => {
+      // LOAD DEFAULT TEMPLATE
+      customStart(microfrontendLayout)
+      console.log(err);
+      return null;
+    })
+  }
+  
+}
 
-applications.forEach(registerApplication);
-layoutEngine.activate();
-start();
+function customStart(layoutHtml){
+  const routes = constructRoutes(layoutHtml);
+  const applications = constructApplications({
+    routes,
+    loadApp({ name }) {
+      return System.import(name);
+    },
+  });
+  const layoutEngine = constructLayoutEngine({ routes, applications });
+
+  applications.forEach(registerApplication);
+  layoutEngine.activate();
+  start();
+}
+
+const template = localStorage.getItem('template')
+
+getTemplate(template)
+
+
